a10884454b472944a304c3f608c6c37b82464416
pepegiallo <nilsherbers@outlook.de>
2024-01-19 16:35:52 +0100

    Removed test.py / launch.json

--- test.py
+++
@@ -1,9 +0,0 @@
-from accessors import execution
-
-if __name__ == '__main__':
-    # Beispiel
-    source = """value = calculate(value, 2)"""
-
-    # Code ausführen und 'value' zurückgeben
-    result = execution.safe_execute(source)
-    print(result)
